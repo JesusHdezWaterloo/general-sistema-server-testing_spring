8e612d9963193c2f89ad2ccd4448a32e0daf5044
Unknown <unknown@example.com>
2020-10-11 15:41:28 -0500

    Agregado spring-a como sub-modulo

--- src/main/java/com/jhw/sistema/server/testing_spring/Application.java
+++ src/main/java/com/jhw/sistema/server/testing_spring/Application.java
@@ -1,11 +1,16 @@
 package com.jhw.sistema.server.testing_spring;
 
+import com.jhw.example.spring_a.rest.A_SpringA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication/*(scanBasePackages = {"com.jhw.example.spring_a.rest"})*/
+@ComponentScan(basePackages = {
+    A_SpringA.BASE_PACKAGE
+})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
