f8e60ee6ee699816d5a099fd1c3501732eef0e95
Unknown <unknown@example.com>
2020-11-06 21:42:17 -0500

    *MISC: +objectMapperBuilder

--- src/main/java/com/jhw/sistema/server/testing_spring/Application.java
+++ src/main/java/com/jhw/sistema/server/testing_spring/Application.java
@@ -2,12 +2,17 @@ package com.jhw.sistema.server.testing_spring;
 
 import com.jhw.module.gestion.contabilidad.rest.A_ModuleGestionContabilidadEmpresarial;
 import com.jhw.module.util.mysql.services.MySQLHandler;
+import com.jhw.utils.jackson.serializer_deserializer.local_date.LocalDateJsonDeserializer;
+import com.jhw.utils.jackson.serializer_deserializer.local_date.LocalDateJsonSerializer;
+import java.time.LocalDate;
 import javax.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication/*(scanBasePackages = {"com.jhw.example.spring_a.rest"})*/
 @ComponentScan(basePackages = {
@@ -30,4 +35,12 @@ public class Application extends SpringBootServletInitializer {
         System.out.println("Cerrando Servicio");
         MySQLHandler.close();
     }
+/*
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.deserializerByType(LocalDate.class, new LocalDateJsonDeserializer());
+        builder.serializerByType(LocalDate.class, new LocalDateJsonSerializer());
+        return builder;
+    }*/
 }
