af8671de15ce939f88a99f0ceb9d95b636b5b930
Unknown <unknown@example.com>
2020-11-08 21:41:45 -0500

    +Modulo Gastos

--- src/main/java/com/jhw/sistema/server/testing_spring/Application.java
+++ src/main/java/com/jhw/sistema/server/testing_spring/Application.java
@@ -1,22 +1,19 @@
 package com.jhw.sistema.server.testing_spring;
 
 import com.jhw.module.gestion.contabilidad.rest.A_ModuleGestionContabilidadEmpresarial;
+import com.jhw.module.gestion.gastos.rest.A_ModuleGestionGastos;
 import com.jhw.module.util.mysql.services.MySQLHandler;
-import com.jhw.utils.jackson.serializer_deserializer.local_date.LocalDateJsonDeserializer;
-import com.jhw.utils.jackson.serializer_deserializer.local_date.LocalDateJsonSerializer;
-import java.time.LocalDate;
 import javax.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication/*(scanBasePackages = {"com.jhw.example.spring_a.rest"})*/
 @ComponentScan(basePackages = {
-    A_ModuleGestionContabilidadEmpresarial.BASE_PACKAGE
+    A_ModuleGestionContabilidadEmpresarial.BASE_PACKAGE,
+    A_ModuleGestionGastos.BASE_PACKAGE
 })
 public class Application extends SpringBootServletInitializer {
 
