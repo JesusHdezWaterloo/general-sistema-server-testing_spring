40fcff63cb02f45d82fc4c64a5cd33323f869bdc
Unknown <unknown@example.com>
2020-12-26 15:54:55 -0500

    Licencia

--- src/main/java/com/jhw/sistema/server/testing_spring/Application.java
+++ src/main/java/com/jhw/sistema/server/testing_spring/Application.java
@@ -1,13 +1,13 @@
 package com.jhw.sistema.server.testing_spring;
 
 import com.jhw.module.admin.kanban.rest.A_ModuleAdminKanban;
-import com.jhw.module.gestion.contabilidad.repo.utils.ResourcesContabilidad;
 import com.jhw.module.admin.kanban.repo.utils.ResourcesKanban;
 import com.jhw.module.admin.seguridad.rest.A_ModuleAdminSeguridad;
 import com.jhw.module.authorization_server.oauth2.A_ModuleOAuth2;
-import com.jhw.module.gestion.contabilidad.rest.A_ModuleGestionContabilidadEmpresarial;
-import com.jhw.module.gestion.gastos.repo.utils.ResourcesGastos;
-import com.jhw.module.gestion.gastos.rest.A_ModuleGestionGastos;
+//import com.jhw.module.gestion.contabilidad.repo.utils.ResourcesContabilidad;
+//import com.jhw.module.gestion.contabilidad.rest.A_ModuleGestionContabilidadEmpresarial;
+//import com.jhw.module.gestion.gastos.repo.utils.ResourcesGastos;
+//import com.jhw.module.gestion.gastos.rest.A_ModuleGestionGastos;
 import com.jhw.module.util.mysql.services.MySQLHandler;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -18,14 +18,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
+import com.jhw.module.util.licence.rest.A_ModuleUtilLicence;
 
 @SpringBootApplication/*(scanBasePackages = {"com.jhw.example.spring_a.rest"})*/
 @ComponentScan(basePackages = {
-    A_ModuleGestionContabilidadEmpresarial.BASE_PACKAGE,
-    A_ModuleGestionGastos.BASE_PACKAGE,
+    //A_ModuleGestionContabilidadEmpresarial.BASE_PACKAGE,
+    //A_ModuleGestionGastos.BASE_PACKAGE,
     A_ModuleAdminKanban.BASE_PACKAGE,
     A_ModuleOAuth2.BASE_PACKAGE,
     A_ModuleAdminSeguridad.BASE_PACKAGE,
+    A_ModuleUtilLicence.BASE_PACKAGE,
 })
 @RestController
 @RequestMapping(value = "/admin")
@@ -35,8 +37,8 @@ public class Application extends SpringBootServletInitializer {
      * Agregar el nombre de las BD a salvar
      */
     private static void save() {
-        MySQLHandler.save(ResourcesContabilidad.SCHEMA);
-        MySQLHandler.save(ResourcesGastos.SCHEMA);
+        //MySQLHandler.save(ResourcesContabilidad.SCHEMA);
+        //MySQLHandler.save(ResourcesGastos.SCHEMA);
         MySQLHandler.save(ResourcesKanban.SCHEMA);
     }
 
